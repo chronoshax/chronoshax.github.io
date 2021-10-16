cbe7572304678571e7532391d025031a9686ec2e
chronoxhax <92594425+chronoshax@users.noreply.github.com>
2021-10-16 13:40:26 +0200

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,68 @@
+/*
+	Cache Strategy: cacheAll (boolean flag)
+		
+		1) cacheAll = true 		=> Cache all requests in urlsToCache list and all further requests
+		2) cacheAll = false 	=> Cache only all requests in urlsToCache list
+*/
+var cacheAll = true;
+var CACHE_NAME = 'chronox-cache';
+var urlsToCache = [
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/index.html',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/logo.png',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/manifest.json',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/js/main.js',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/css/main.css',
+	'https://raw.githubusercontent.com/chronoshax/chronoshax.github.io/hax/js/jquery.min.js'
+];
+
+// Install Event
+self.addEventListener('install', function(event) {
+	console.log("[SW] install event: ",event);
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(
+			function(cache) {
+				console.log('[SW] Opened cache: ',cache);
+				return cache.addAll(urlsToCache);
+			}
+		)
+	);
+});
+
+
+// Fetch Event
+self.addEventListener('fetch', function(event) {
+	console.log("[SW] fetch event: ",event);
+	event.respondWith(
+		caches.match(event.request).then(
+			function(response) {
+				// Cache hit - return response
+				if (response) return response;
+				// What cache strategy should be used? => cacheAll (boolean flag)
+				else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
+				else {
+					fetch(event.request).then(
+						// Try to cache new requests directly 
+						function(response) {
+							// Check if we received a valid response
+							if(!response || response.status !== 200 || response.type !== 'basic') return response;
+							// IMPORTANT: Clone the response. A response is a stream
+							// and because we want the browser to consume the response
+							// as well as the cache consuming the response, we need
+							// to clone it so we have two streams.
+							var responseToCache = response.clone();
+							caches.open(CACHE_NAME).then(
+								function(cache) {
+									cache.put(event.request, responseToCache);
+								}
+							);
+							return response;
+						}
+					);
+				}
+			}
+		)
+	);
+});
+
